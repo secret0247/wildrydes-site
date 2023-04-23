d2e804ab1e9c9a9d5d7dc738ba6b77ef6045752b
secret0247 <129030207+secret0247@users.noreply.github.com>
2023-04-23 03:11:38 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_yie5EpOnM', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '23h0mitm6r8l67br6opcitegqh0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
